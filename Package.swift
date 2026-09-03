9873dae4fea66dec6e7d17098a37ad54847203d5
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-09-03 15:03:40 +0200

    Converge dependency locations to their canonical organizations

--- Package.swift
+++ Package.swift
@@ -1,25 +1,6 @@
 // swift-tools-version: 6.4
 import PackageDescription
 
-extension String {
-    static let bcp47: Self = "BCP 47"
-    static let bcp47Tests: Self = bcp47 + " Tests"
-
-    static let rfc5646: Self = "RFC 5646"
-
-    var tests: Self { self + " Tests" }
-}
-
-extension PackageDescription.Target.Dependency {
-    static let rfc5646: Self = .product(name: "RFC 5646", package: "swift-rfc-5646")
-    static let standards: Self = .product(
-        name: "Standard Library Extensions",
-        package: "swift-standard-library-extensions"
-    )
-
-    static let bcp47: Self = .target(name: .bcp47)
-}
-
 let package = Package(
     name: "swift-bcp-47",
     platforms: [
@@ -42,14 +23,14 @@ let package = Package(
         .target(
             name: "BCP 47",
             dependencies: [
-                .standards,
-                .rfc5646,
+                .product(name: "Standard Library Extensions", package: "swift-standard-library-extensions"),
+                .target(name: "RFC 5646"),
             ]
         ),
         .testTarget(
             name: "BCP 47 Tests",
             dependencies: [
-                "BCP 47"
+                .target(name: "BCP 47")
             ]
         ),
     ],
